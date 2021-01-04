c9eb5aeb81cf8c643fdafcefb73ce25520cecb1f
Falko Joseph <falkojoseph@gmail.com>
2021-01-04 14:48:57 +0100

    update open home bug

--- modules/update/class-open-homes-update.php
+++ modules/update/class-open-homes-update.php
@@ -6,30 +6,36 @@
  * @package FieldOpenHomesUpdate
  */
 
-class FieldOpenHomesUpdate {
+class FieldOpenHomesUpdate
+{
 
-	public function __construct() {
+	public function __construct()
+	{
 	}
 
-	public static function update($estate, $post_id) {
-    $open_homes = [];
-	  foreach ($estate['open_homes'] as $key => $open_home) {
-	    $open_homes[] = [
-	      'acf_fc_layout' => 'open_home_layout',
-	      'open_homes_start_date' => $open_home['start_date'],
-	      'open_homes_end_date' => $open_home['end_date'],
-	    ];
+	public static function update($estate, $post_id)
+	{
+		$open_homes = [];
+		foreach ($estate['open_homes'] as $key => $open_home) {
+			$open_homes[] = [
+				'acf_fc_layout' => 'open_home_layout',
+				'open_homes_start_date' => $open_home['start_date'],
+				'open_homes_end_date' => $open_home['end_date'],
+			];
 		}
 
 		if (count($open_homes) > 0) {
 			update_field('open_homes', [
 				'hasOpenHome' => true,
 			], $post_id);
+		} else {
+			update_field('open_homes', [
+				'hasOpenHome' => false,
+			], $post_id);
 		}
 
-	  update_field('open_homes', [
-	    'open_home_date' => $open_homes,
-    ], $post_id);
+		update_field('open_homes', [
+			'open_home_date' => $open_homes,
+		], $post_id);
 	}
-
 }
