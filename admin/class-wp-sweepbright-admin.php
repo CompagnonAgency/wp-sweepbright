d2d635263275400b5d4ff96c7cd3df42100f3352
Falko Joseph <falkojoseph@gmail.com>
2024-12-10 13:21:16 +0100

    fix

--- admin/class-wp-sweepbright-admin.php
+++ admin/class-wp-sweepbright-admin.php
@@ -244,7 +244,7 @@ class WP_SweepBright_Admin
 			$this->plugin_name . '-properties',
 			'Contact',
 			'Contact',
-			'publish_pages',
+			'activate_plugins',
 			$this->plugin_name . '-contact',
 			array($this, 'load_admin_contact'),
 		);
@@ -254,7 +254,7 @@ class WP_SweepBright_Admin
 			$this->plugin_name . '-properties',
 			'Settings',
 			'Settings',
-			'publish_pages',
+			'activate_plugins',
 			$this->plugin_name . '-settings',
 			array($this, 'load_admin_settings'),
 		);
